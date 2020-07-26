6b2954e1df76c3731d0ff54a1b73818263612aed
Ludwig Boess <di67meg@c2paplogin.cos.lrz.de>
2020-07-26 10:17:56 +0200

    Copy&pasted version from SPHKernels.jl

--- New_Define/test.jl
+++ New_Define/test.jl
@@ -4,6 +4,16 @@ Pkg.activate(".")
 
 using BenchmarkTools
 
+"""
+    struct WendlandC6 <: SPHKernel
+        n_neighbours::Integer
+        norm_2D::Float64
+        norm_3D::Float64
+        function WendlandC6(n_neighbours::Integer=295)
+            new(n_neighbours, 78.0/(7.0*π), 1365.0/(64.0*π))
+        end
+    end
+"""
 struct WendlandC6
     n_neighbours::Int64
     norm_2D::Float64
@@ -16,9 +26,9 @@ end
 """
     kernel_value_2D(kernel::WendlandC6, u::Float64, h_inv::Float64)
 
-Evaluate WendlandC6 spline at position ``u = \frac{x}{h}``.
+Evaluate WendlandC6 spline at position ``u = \\frac{x}{h}``.
 """
-@inline function kernel_value(kernel::WendlandC6, u::Float64, h_inv::Float64)
+@inline function kernel_value_2D(kernel::WendlandC6, u::Float64, h_inv::Float64)
 
     @fastmath if u < 1.0
         n = kernel.norm_2D * h_inv^2
@@ -29,11 +39,11 @@ Evaluate WendlandC6 spline at position ``u = \frac{x}{h}``.
         u2 = u*u
         return ( u_m1 * ( 1.0 + 8u + 25u2 + 32u2*u )) * n
     else
-       return 0.0
-   end
+        return 0.0
+    end
 
 end
 
 k = WendlandC6()
-@btime kernel_value(k, 0.5, 0.5)
-@btime kernel_value(k, 0.5, 0.5)
+@btime kernel_value_2D(k, 0.5, 0.5)
+@btime kernel_value_2D(k, 0.5, 0.5)
